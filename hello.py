0098bc4a562b87b5d6196640c787973528f59529
Robin Burri <rburri@c1r16s5.42lausanne.ch>
2023-08-24 15:11:31 +0200

    add decorators

--- hello.py
+++ hello.py
@@ -2,6 +2,33 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+def make_bold(function):
+    def wrapper():
+        return f"<b>{function()}</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return f"<em>{function()}</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return f"<u>{function()}</u>"
+    return wrapper
+
+
 @app.route("/")
+@make_bold
+@make_emphasis
+@make_underlined
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "Hello, World!"
+
+@app.route("/user/<name>")
+def user(name):
+    return f"<p>Hello, {name}!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
